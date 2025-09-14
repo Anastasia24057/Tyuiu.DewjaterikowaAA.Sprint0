0a473be19a953e5e9bbad9d581a9252e968548a0
Анастасия Девятерикова <Tyuiu.DewjaterikowaAA@outlook.com>
2025-09-14 17:26:59 +0300

    Task6

---
+++ Tyuiu.DewjaterikowaAA.Sprint0.Task6.V0.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.DewjaterikowaAA.Sprint0.Task6.V0.Lib
+{
+    public class DataService
+    {
+        public static object AdditionArray(int[] numbers)
+        {
+            var total = 0;
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                total = total + numbers[i];
+            }
+            return total;
+        }
+    }
+}
---
+++ Tyuiu.DewjaterikowaAA.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.DewjaterikowaAA.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.DewjaterikowaAA.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataServiceTest.AdditionArray(numbers);
+            Assert.AreEqual(15, res);
+        }
+    }
+}
